1897167c365ceac3b19207af0736950579486afd
jhembe <jhembe202@gmail.com>
2023-05-07 03:17:30 +0300

    Modified Hero Section

---
+++ app.js
@@ -0,0 +1,13 @@
+let docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+    else{
+        console.log("None element overflows")
+    }
+  }
+);
