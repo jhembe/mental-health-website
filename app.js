854dc5a3969f351da927f6dde7ac7f53c4b5f5d9
jhembe <jhembe202@gmail.com>
2023-05-11 19:59:22 +0300

    added some animations

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
-let docWidth = document.documentElement.offsetWidth;
+// let docWidth = document.documentElement.offsetWidth;
 
-[].forEach.call(
-  document.querySelectorAll('*'),
-  function(el) {
-    if (el.offsetWidth > docWidth) {
-      console.log(el);
-    }
-    else{
-        console.log("None element overflows")
-    }
-  }
-);
+// [].forEach.call(
+//   document.querySelectorAll('*'),
+//   function(el) {
+//     if (el.offsetWidth > docWidth) {
+//       console.log(el);
+//     }
+//     else{
+//         console.log("None element overflows")
+//     }
+//   }
+// );
+
+
+$(document).ready(function () {
+  let offset = {offset:'50%'}
+  console.log("Loaded Successfuly")
+  $('.hero-heading').waypoint(function(){
+    // console.log("Reached to top");
+    $('.hero-heading').addClass('animate__animated animate__pulse')
+  })
+
+  $('.about-heading').waypoint(function(){
+    $('.about-heading').addClass('animate__animated animate__bounce')
+  },
+  {offset:'50%'})
+
+  $('.founders-heading').waypoint(function(){
+    $('.founders-heading').addClass('animate__animated animate__fadeInLeft')
+  },
+  {
+    offset:'50%'
+  })
+
+});
